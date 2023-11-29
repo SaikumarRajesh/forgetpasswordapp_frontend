619dcd6e0d8d7be6a4de595b93aaeaccbf5d88d0
saikumar <saikumarmech12@gmail.com>
2023-11-29 18:40:49 +0530

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -74,7 +74,7 @@ function Login() {
         <button type="submit" >Forget_Password</button>
       </form>
       <br/>
-      <a href='/register'> If dont have an account? Click here to Register</a>
+      <a href='http://localhost:5173/register'> If dont have an account? Click here to Register</a>
       </div>
       </div>
     );
